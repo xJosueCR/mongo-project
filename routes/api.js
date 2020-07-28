daa6bf2b6de0e63f4e9cadb41aa06cc1d6fef8a6
Josue <josue.cespedes19@gmail.com>
2020-07-28 13:06:15 -0600

    Second commit

--- routes/api.js
+++ routes/api.js
@@ -1,33 +1,37 @@
 // Full Documentation - https://docs.turbo360.co
-const express = require('express')
-const { response } = require('../app')
-const router = express.Router()
- //importing profile js
- const profile = require('../models/profile')
+const express = require('express');
+const { response } = require('../app');
+const router = express.Router();
+//importing profile js
+const profile = require('../models/profile');
 const mongoose = require('mongoose');
 /*  This is a sample API route. */
-//get handler 
-mongoose.connect("mongodb://localhost/test", function (err) {
-    if (err) {
-      console.err(err);
-    } else {
-      console.log("Connected");
-    }
+//get handler
+mongoose.connect('mongodb://localhost/test', function (err) {
+  if (err) {
+    console.err(err);
+  } else {
+    console.log('Connected');
+  }
 });
-router.get('/profile', (request, response)=>{
+router.get('/profile', (request, response) => {
   const query = request.query;
-  profile.find(query).then(
-    profiles => {
+  profile
+    .find(query)
+    .then((profiles) => {
       response.json({
         confirmation: 'success',
-        data: profiles
-      })
-  }).catch(err =>{
-    response.json({
-      confirmation: 'Failed',
-      message: 'An error has ocurred'
+        data: profiles,
+      });
     })
-  })
-})
+    .catch((err) => {
+      response.json({
+        confirmation: 'Failed',
+        message: 'An error has ocurred',
+      });
+    });
+});
 
-module.exports = router
+module.exports = router;
+
+//hello
